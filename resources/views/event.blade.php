8246f167e32f7bf11efc2dd674242a853471d881
unknown <abellamechaela@gmail.com>
2026-02-18 12:52:09 +0800

    ass6

---
+++ resources/views/event.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Event Registration</title>
+</head>
+<body>
+    <h1>Event Registration Page</h1>
+
+    <p><strong>Event Name:</strong> {{ $event }}</p>
+    <p><strong>Participant Name:</strong> {{ $participant }}</p>
+    <p><strong>Year Level:</strong> {{ $yearLevel }}</p>
+</body>
+</html>
